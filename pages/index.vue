913e644d299e47d366cab48b18a779b439f82963
tianyong90 <412039588@qq.com>
2019-05-13 19:21:25 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <section class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        {{ msg }}
+      </h1>
+      <button class="btn btn-primary">asdfas</button>
+      <h2 class="subtitle">
+        My splendid Nuxt.js project
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+          >GitHub</a
+        >
+      </div>
+
+      <div v-html="test"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import test from '../posts/test.md'
+
+export default {
+  components: {
+    Logo
+  },
+
+  data() {
+    return {
+      msg: 'hello haha',
+      test: test
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
