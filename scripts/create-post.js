c83627a9ace5b07b48eef68f26c9fc052c75bd40
tianyong90 <412039588@qq.com>
2019-07-07 21:43:22 +0800

    新建文章命令

--- scripts/create-post.js
+++ scripts/create-post.js
@@ -1,42 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const yargs = tslib_1.__importStar(require("yargs"));
-// eslint-disable-next-line
-const rControl = /[\u0000-\u001f]/g;
-const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'<>,.?/]+/g;
-const titleOptions = {
-    description: 'title',
-    demand: true,
-    alias: 't',
-};
-const argv = yargs
-    .command('add', 'add TODO to database', {
-    title: titleOptions,
+const fs_extra_1 = tslib_1.__importDefault(require("fs-extra"));
+const yargs_1 = tslib_1.__importDefault(require("yargs"));
+const gray_matter_1 = tslib_1.__importDefault(require("gray-matter"));
+const argv = yargs_1.default
+    .command('new', 'create new post', {
+    title: {
+        description: 'post title',
+        demand: true,
+        alias: 't',
+    },
 })
     .help('h').argv;
-// // 标题
-// const title = yargs.argv
-// console.log(title)
-// if (!title) {
-//   throw new Error('标题不能为空')
-// }
-// // 标题空格转 -
-// const slugTitle = title.replace(rControl, '').replace(rSpecial, '-')
-// // 生成目录
-// fs.mkdirp(`./posts/${slugTitle}`)
-// const frontmatterData = {
-//   title: title,
-//   date: new Date()
-//     .toISOString()
-//     .replace(/T/, ' ')
-//     .replace(/\..+/, ''),
-//   top_img: '',
-//   tags: [''],
-//   categories: [''],
-// }
-// // 生成 front-matter yml 格式内容
-// const mdContent = matter.stringify('', frontmatterData)
-// console.log(mdContent)
-// // 生成 markdown 文件
-// fs.writeFile(`./posts/${slugTitle}.md`, mdContent)
+if (argv._[0] === 'new') {
+    // 标题
+    const title = argv.title;
+    if (!title) {
+        throw new Error('标题不能为空');
+    }
+    // eslint-disable-next-line
+    const rControl = /[\u0000-\u001f]/g;
+    const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'<>,.?/]+/g;
+    // 标题空格转 -
+    const slugTitle = title.replace(rControl, '').replace(rSpecial, '-');
+    // 生成目录
+    fs_extra_1.default.mkdirp(`./posts/${slugTitle}`);
+    const frontmatterData = {
+        title,
+        date: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        top_img: '',
+        tags: [''],
+        categories: [''],
+    };
+    // 生成 front-matter yml 格式内容
+    const mdContent = gray_matter_1.default.stringify('', frontmatterData);
+    // 生成 markdown 文件
+    fs_extra_1.default.writeFile(`./posts/${slugTitle}.md`, mdContent);
+    console.info('新建成功');
+}
--- scripts/create-post.ts
+++ scripts/create-post.ts
@@ -1,54 +1,52 @@
 import path from 'path'
 import fs from 'fs-extra'
-import * as yargs from 'yargs'
+import yargs from 'yargs'
 import matter from 'gray-matter'
 
-// eslint-disable-next-line
-const rControl = /[\u0000-\u001f]/g
-const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'<>,.?/]+/g
-
-const titleOptions: yargs.Options = {
-  description: 'title',
-  demand: true,
-  alias: 't',
-}
-
 const argv = yargs
-  .command('add', 'add TODO to database', {
-    title: titleOptions,
+  .command('new', 'create new post', {
+    title: {
+      description: 'post title',
+      demand: true,
+      alias: 't',
+    },
   })
   .help('h').argv
 
-// // 标题
-// const title = yargs.argv
+if (argv._[0] === 'new') {
+  // 标题
+  const title = argv.title as string
 
-// console.log(title)
+  if (!title) {
+    throw new Error('标题不能为空')
+  }
 
-// if (!title) {
-//   throw new Error('标题不能为空')
-// }
+  // eslint-disable-next-line
+  const rControl = /[\u0000-\u001f]/g
+  const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'<>,.?/]+/g
 
-// // 标题空格转 -
-// const slugTitle = title.replace(rControl, '').replace(rSpecial, '-')
+  // 标题空格转 -
+  const slugTitle = title.replace(rControl, '').replace(rSpecial, '-')
 
-// // 生成目录
-// fs.mkdirp(`./posts/${slugTitle}`)
+  // 生成目录
+  fs.mkdirp(`./posts/${slugTitle}`)
 
-// const frontmatterData = {
-//   title: title,
-//   date: new Date()
-//     .toISOString()
-//     .replace(/T/, ' ')
-//     .replace(/\..+/, ''),
-//   top_img: '',
-//   tags: [''],
-//   categories: [''],
-// }
+  const frontmatterData = {
+    title,
+    date: new Date()
+      .toISOString()
+      .replace(/T/, ' ')
+      .replace(/\..+/, ''),
+    top_img: '',
+    tags: [''],
+    categories: [''],
+  }
 
-// // 生成 front-matter yml 格式内容
-// const mdContent = matter.stringify('', frontmatterData)
+  // 生成 front-matter yml 格式内容
+  const mdContent = matter.stringify('', frontmatterData)
 
-// console.log(mdContent)
+  // 生成 markdown 文件
+  fs.writeFile(`./posts/${slugTitle}.md`, mdContent)
 
-// // 生成 markdown 文件
-// fs.writeFile(`./posts/${slugTitle}.md`, mdContent)
+  console.info('新建成功')
+}
