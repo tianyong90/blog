87092ce37adba8ce95b30cc54293db2d45f022b4
tianyong90 <412039588@qq.com>
2019-09-21 01:26:39 +0800

    上一篇下一篇按钮

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="mx-auto my-0 md:my-4 max-w-4xl post">
-    <img :src="topImg" class="w-full cover-image" />
+  <div class="mx-auto my-0 md:my-4 max-w-4xl">
+    <div class="post">
+      <img :src="topImg" class="w-full cover-image" />
 
-    <div class="px-4 md:px-8 py-4">
-      <div class="mb-4">
-        <h1 class="text-gray-800 text-2xl font-normal" v-text="title" />
-        <div class="text-gray-700 text-xs post-date">{{ date | formatTime }}</div>
+      <div class="px-4 md:px-8 py-4">
+        <div class="mb-4">
+          <h1 class="text-gray-800 text-2xl font-normal" v-text="title" />
+          <div class="text-gray-700 text-xs post-date">{{ date | formatTime }}</div>
+        </div>
+
+        <div class="markdown-body" v-html="html" />
+
+        <div class="social-share"></div>
       </div>
+    </div>
 
-      <div class="markdown-body" v-html="html" />
-
-      <div class="social-share"></div>
+    <div class="navigator">
+      <nuxt-link v-if="prevLink" tag="a" :to="prevLink" class="navigator-btn mr-auto btn-prev"
+        ><span class="mdi mdi-chevron-left"></span> 上一篇
+      </nuxt-link>
+      <nuxt-link v-if="nextLink" tag="a" :to="nextLink" class="navigator-btn ml-auto btn-next"
+        >下一篇<span class="mdi mdi-chevron-right"></span
+      ></nuxt-link>
     </div>
   </div>
 </template>
@@ -18,22 +29,14 @@
 <script lang="ts">
 import Vue from 'vue'
 import dayjs from 'dayjs'
-// TODO:
-// import 'social-share.js/dist/css/share.min.css'
-// import SocialShare from 'social-share.js'
-
-// interface options extends Vue {
-//   tags: any
-// }
+import { orderBy } from 'lodash'
 
 export default Vue.extend({
   head() {
     return {
       title: (this as any).title,
       meta: [
-        // TODO: any
         { hid: 'keywords', name: 'keywords', content: (this as any).tags.join(',') },
-        // TODO: 关键词等
         { hid: 'description', name: 'description', content: '' },
       ],
     }
@@ -57,7 +60,10 @@ export default Vue.extend({
   },
 
   async asyncData({ params }) {
-    const { default: posts } = await import('~/posts/posts.json')
+    let { default: posts } = await import('~/posts/posts.json')
+
+    // 按发布时间排序
+    posts = orderBy(posts, 'date', 'desc')
 
     // 链接中拼音化的文件名
     const slugifiedFilename = params.slug
@@ -65,6 +71,15 @@ export default Vue.extend({
     const thePost: any = posts.find((item: any) => {
       return item.slugifiedFilename === slugifiedFilename
     })
+
+    // 上一篇和下一篇
+    const thePostIndex = posts.indexOf(thePost)
+
+    const prevPost = thePostIndex === 0 ? null : posts[thePostIndex - 1]
+    const nextPost = thePostIndex === posts.length - 1 ? null : posts[thePostIndex + 1]
+
+    const prevLink = prevPost ? `/posts/${prevPost.slugifiedFilename}` : null
+    const nextLink = nextPost ? `/posts/${nextPost.slugifiedFilename}` : null
 
     // posts 目录中 markdown 实际文件名
     const filename = thePost.filename
@@ -81,38 +96,47 @@ export default Vue.extend({
       ...attributes,
       topImg,
       html: html.replace(/src="\.\//g, `src="/_nuxt/posts/${filename}/`), // markdown 内容中图片地址引用替换
+      prevLink,
+      nextLink,
     }
   },
 
-  async mounted() {
-    // TODO:
-    // SocialShare('.social-share', {
-    //   title: 'abc'
-    // })
-  },
+  async mounted() {},
 })
 </script>
 
 <style scoped lang="scss">
-.cover-image {
-  max-height: 420px;
-  object-fit: cover;
-}
-
 .post {
   background-color: rgba(255, 255, 255, 0.65);
 }
 
-.post-head-wrapper {
-  .post-title,
-  .post-date {
-    text-shadow: 0 0 10px rgba(0, 0, 0, 0.65);
-  }
+.cover-image {
+  max-height: 420px;
+  object-fit: cover;
 }
 
 .markdown-body {
   min-height: 40vh;
   font-size: 1rem;
   text-align: justify;
+}
+
+.navigator {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  margin-top: 2rem;
+
+  &-btn {
+    font-size: 0.85rem;
+    background-color: rgba(255, 255, 255, 0.65);
+    padding: 0.5rem 1rem;
+    border-radius: 0.2rem;
+    text-decoration: none;
+
+    &:hover {
+      background-color: darken(rgba(255, 255, 255, 0.65), 20);
+    }
+  }
 }
 </style>
