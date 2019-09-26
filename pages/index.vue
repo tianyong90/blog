22c361c9ab62774abe925d4c6d42f90e457621cc
tianyong90 <412039588@qq.com>
2019-09-26 12:32:08 +0800

    keywords, description

--- pages/index.vue
+++ pages/index.vue
@@ -79,7 +79,14 @@ export default Vue.extend({
   head() {
     return {
       title: '首页',
-      // TODO: keyword and description
+      meta: [
+        { hid: 'keywords', name: 'keywords', content: '田写，田勇的博客，tianyong90,田较瘦' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: '田勇的个人博客，技术、分享、生活及其它',
+        },
+      ],
     }
   },
 
