96995cbe10b759ab1a633f71308598ba86447d3d
tianyong90 <412039588@qq.com>
2020-03-23 13:28:04 +0800

    fusejs option

--- components/Header.vue
+++ components/Header.vue
@@ -101,7 +101,6 @@ export default class Header extends Vue {
       threshold: 0.6,
       location: 0,
       distance: 100,
-      maxPatternLength: 32,
       minMatchCharLength: 1,
       keys: ['title'],
     }
