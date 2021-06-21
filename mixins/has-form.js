b1f8c79f1c2b836c1c993406026b7f6118c98182
tianyong90 <412039588@qq.com>
2021-06-21 15:49:09 +0800

    yarn upgrade

--- mixins/has-form.js
+++ mixins/has-form.js
@@ -1,23 +1,7 @@
 import Vue from 'vue'
 import { mapState } from 'vuex'
-import { ValidationObserver, ValidationProvider, extend } from 'vee-validate'
-
-import * as rules from 'vee-validate/dist/rules'
-import { messages } from 'vee-validate/dist/locale/en.json'
-
-Object.keys(rules).forEach((rule) => {
-  extend(rule, {
-    ...rules[rule],
-    message: messages[rule],
-  })
-})
 
 export default Vue.extend({
-  components: {
-    ValidationObserver,
-    ValidationProvider,
-  },
-
   data () {
     return {}
   },
